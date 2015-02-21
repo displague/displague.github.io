ea63ad7c201ba895874807468d4343a8bff8ccd5
Marques Johansson <github@displague.com>
2015-02-21 21:59:24 +0000

    list some repos

---
+++ app.js
@@ -0,0 +1,21 @@
+(function(angular){
+  'use strict';
+  var app = angular.module('DisplagueGHIOApp', ['ngMaterial', 'ngResource']);
+  
+  app.factory('ghRepos', ['$resource', function($resource){
+    return $resource('https://api.github.com/users/displague/repos', {},
+    {
+      query: {method:'GET', isArray:true}
+    });
+  }]);
+  
+  app.controller('RepoListCtrl', ['$scope', '$mdSidenav', 'ghRepos', function($scope, $mdSidenav, ghRepos){
+      //.complete(function(data){
+    ghRepos.query({},function(data){$scope.repos=data});
+    
+    $scope.toggleSidenav = function(menuId) {
+      $mdSidenav(menuId).toggle();
+    };
+  }]);
+  
+})(angular);
